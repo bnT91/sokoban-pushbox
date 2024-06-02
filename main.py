81d0f5eda3c2c53ea9679b6e3095a3d6e39a2874
bnT91 <151850289+bnT91@users.noreply.github.com>
2024-06-02 21:29:29 +0300

    updated textures, first level show

--- main.py
+++ main.py
@@ -9,12 +9,19 @@
 
 import pygame as pg
 import sys
+import json
+
+# Levels
+with open("levels.json", "r") as f:
+    levels = json.load(f)
 
 # Init
 pg.init()
+clock = pg.time.Clock()
 
 # Images
 icon = pg.image.load("pic/wall.png")
+wall = icon
 
 # Screen
 scr = pg.display.set_mode((8*32, 8*32))
@@ -22,7 +29,14 @@ pg.display.set_caption("PushBoxPush")
 pg.display.set_icon(icon)
 
 while True:
+    for x in range(8):
+        for y in range(8):
+            if levels[0][x][y] == "#":
+                scr.blit(wall, (x*32, y*32))
     for ev in pg.event.get():
         if ev.type == pg.QUIT:
             pg.quit()
             sys.exit()
+
+    pg.display.flip()
+    clock.tick(20)
