e4035c53a591d34c33d97683e6f4553829b7799e
bnT91 <151850289+bnT91@users.noreply.github.com>
2024-06-02 21:07:01 +0300

    wall.png, empty_level.png, process

--- main.py
+++ main.py
@@ -7,3 +7,22 @@
 # T - Цель для ящика
 # P - Стартовая позиция игрока
 
+import pygame as pg
+import sys
+
+# Init
+pg.init()
+
+# Images
+icon = pg.image.load("pic/wall.png")
+
+# Screen
+scr = pg.display.set_mode((8*32, 8*32))
+pg.display.set_caption("PushBoxPush")
+pg.display.set_icon(icon)
+
+while True:
+    for ev in pg.event.get():
+        if ev.type == pg.QUIT:
+            pg.quit()
+            sys.exit()
